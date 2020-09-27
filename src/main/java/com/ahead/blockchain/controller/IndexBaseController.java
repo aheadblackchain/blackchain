4eaf03781f9633cd4dfaae72b5b4d88e050ef890
kanya <qingxingmeixiang@163.com>
2020-09-27 11:33:37 +0800

    page info

--- src/main/java/com/ahead/blockchain/controller/IndexBaseController.java
+++ src/main/java/com/ahead/blockchain/controller/IndexBaseController.java
@@ -19,4 +19,28 @@ public class IndexBaseController {
     public String info(){
         return "website/blackchaininfo";
     }
+
+    @GetMapping("/top")
+    public String top(){
+        return "website/top";
+    }
+    @GetMapping("/footer")
+    public String footer(){
+        return "website/footer";
+    }
+
+    @GetMapping("/curriculumInfo")
+    public String curriculumInfo(){
+        return "website/curriculuminfo";
+    }
+
+    @GetMapping("/projectInfo")
+    public String projectInfo(){
+        return "website/projectinfo";
+    }
+
+    @GetMapping("/newInfo")
+    public String newInfo(){
+        return "website/newinfo";
+    }
 }
